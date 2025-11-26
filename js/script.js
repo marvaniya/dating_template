7b6a79cecbc3cc8e4b089ae7e1eaadb687bf5cb7
Dharmik Marvaniya <dharmikmarvaniya123@gmail.com>
2025-11-26 22:59:48 +0530

    code update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,37 @@ $(".toggle_btn").click(function(){
 $(".close_menu").click(function(){
     $(".header_menu").css("left","-100%")
 })
+
+function startCount() {
+    $('.count_box h3').each(function () {
+        let $this = $(this);
+        let original = $this.text();       
+        let num = parseFloat(original);       
+        let suffix = original.replace(num, ""); 
+        
+        $({ countNum: 0 }).animate(
+            { countNum: num },
+            {
+                duration: 2000,
+                easing: "swing",
+                step: function () {
+                    $this.text(Math.floor(this.countNum) + suffix);
+                },
+                complete: function () {
+                    $this.text(num + suffix);
+                }
+            }
+        );
+    });
+}
+
+let counted = false;
+
+$(window).on('scroll', function () {
+    let sectionTop = $('.index_eleventh_section').offset().top - window.innerHeight;
+
+    if (!counted && $(window).scrollTop() > sectionTop) {
+        startCount();
+        counted = true; 
+    }
+});
